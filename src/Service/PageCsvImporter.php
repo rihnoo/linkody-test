1c95e421b8585c323ae00700a02cd611771349a2
rihnoo <minglee90611@gmail.com>
2022-10-24 04:50:35 +0800

    logging

--- src/Service/PageCsvImporter.php
+++ src/Service/PageCsvImporter.php
@@ -3,15 +3,18 @@
 namespace App\Service;
 
 use Doctrine\ORM\EntityManager;
+use Psr\Log\LoggerInterface;
 use App\Entity\Page;
 
 class PageCsvImporter
 {
     private EntityManager $entityManager;
+    private LoggerInterface $logger;
 
-    public function __construct(EntityManager $entityManager)
+    public function __construct(EntityManager $entityManager, LoggerInterface $logger)
     {
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
 
     public function import(String $filename): void
@@ -19,12 +22,16 @@ class PageCsvImporter
         // To parse the csv file that contains more than 1 million lines, my approach is to use Generator
         $generator = $this->readCSV($filename);
 
-        foreach ($generator as $item) {
-            $page = new Page();
-            $page->setUrl($item['URL']);
+        try {
+            foreach ($generator as $item) {
+                $page = new Page();
+                $page->setUrl($item['URL']);
 
-            $this->entityManager->persist($page);
-            $this->entityManager->flush();
+                $this->entityManager->persist($page);
+                $this->entityManager->flush();
+            }
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
         }
     }
 
